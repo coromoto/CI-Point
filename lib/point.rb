8b6362831148418c5c3851b5ee17a4694cde7e3b
Coromoto Leon <cleon@ull.edu.es>
2013-10-21 10:21:17 -0400

    Cumplida la espectaiva del almacenamiento de las coordenadas
    $rake
    rspec -I. spec/point_spec.rb
    ..
    
    Finished in 0.01702 seconds
    2 examples, 0 failures

--- lib/point.rb
+++ lib/point.rb
@@ -1,2 +1,6 @@
 class Point
+  attr_reader :x, :y
+  def initialize(x,y)
+    @x, @y = x, y
+  end
 end
--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -1,6 +1,10 @@
 require "lib/point.rb"
 
 describe Point do
+  before :each do
+    @p1 = Point.new(1,1)
+  end    
+    
   describe "# almacenamiento de las coordenadas" do
     it "Se almacena correctamente la coordenada X" do
       @p1.x.should eq(1) 
