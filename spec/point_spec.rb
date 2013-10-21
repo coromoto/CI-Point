00fe7d8ff4381f3b2154eeccce8ba43faeb787de
Coromoto Leon <cleon@ull.edu.es>
2013-10-21 07:53:15 -0400

    Se crea una expectativa para almacenar y acceder a las coordenadas de un punto
    $ rake
    rspec -I. spec/point_spec.rb
    FF
    
    Failures:
    
      1) Point# almacenamiento de las coordenadas Se almacena correctamente la coordenada X
         Failure/Error: @p1.x.should eq(1)
         NoMethodError:
           undefined method `x' for nil:NilClass
         # ./spec/point_spec.rb:6:in `block (3 levels) in <top (required)>'
    
      2) Point# almacenamiento de las coordenadas Se almacena correctamente la coordenada Y
         Failure/Error: @p1.y.should eq(1)
         NoMethodError:
           undefined method `y' for nil:NilClass
         # ./spec/point_spec.rb:9:in `block (3 levels) in <top (required)>'
    
    Finished in 0.0204 seconds
    2 examples, 2 failures
    
    Failed examples:
    
    rspec ./spec/point_spec.rb:5 # Point# almacenamiento de las coordenadas Se almacena correctamente la coordenada X
    rspec ./spec/point_spec.rb:8 # Point# almacenamiento de las coordenadas Se almacena correctamente la coordenada Y
    rake aborted!
    Command failed with status (1): [rspec -I. spec/point_spec.rb...]
    
    Tasks: TOP => default => spec
    (See full trace by running task with --trace)

--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -1,5 +1,12 @@
 require "lib/point.rb"
 
 describe Point do
-    
+  describe "# almacenamiento de las coordenadas" do
+    it "Se almacena correctamente la coordenada X" do
+      @p1.x.should eq(1) 
+    end
+    it "Se almacena correctamente la coordenada Y" do
+      @p1.y.should eq(1)  
+    end
+  end
 end
