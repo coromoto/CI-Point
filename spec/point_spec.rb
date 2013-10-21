960da66ab63d6fba96b53f28467131f1ccd761be
Coromoto Leon <cleon@ull.edu.es>
2013-10-21 10:30:55 -0400

    Incluida una descripcion con dos espectativas para el productor escalar
    $ rake
    rspec -I. spec/point_spec.rb
    ..FF
    
    Failures:
    
      1) Point# producto por un escalar la coordenada x y la coordenada y se multiplican por el valor
         Failure/Error: p2 = @p1*5
         NoMethodError:
           undefined method `*' for #<Point:0x00000001000b08 @x=1, @y=1>
         # ./spec/point_spec.rb:19:in `block (3 levels) in <top (required)>'
    
      2) Point# producto por un escalar Si se multiplica por -1 se obtiene el opuesto
         Failure/Error: p2 = @p1*-1
         NoMethodError:
           undefined method `*' for #<Point:0x00000000ffe448 @x=1, @y=1>
         # ./spec/point_spec.rb:24:in `block (3 levels) in <top (required)>'
    
    Finished in 0.00693 seconds
    4 examples, 2 failures
    
    Failed examples:
    
    rspec ./spec/point_spec.rb:18 # Point# producto por un escalar la coordenada x y la coordenada y se multiplican por el valor
    rspec ./spec/point_spec.rb:23 # Point# producto por un escalar Si se multiplica por -1 se obtiene el opuesto
    rake aborted!
    Command failed with status (1): [rspec -I. spec/point_spec.rb...]
    
    Tasks: TOP => default => spec
    (See full trace by running task with --trace)

--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -13,4 +13,17 @@ describe Point do
       @p1.y.should eq(1)  
     end
   end
+  
+  describe "# producto por un escalar" do
+    it "la coordenada x y la coordenada y se multiplican por el valor" do
+      p2 = @p1*5 
+      p2.x.should eq(5)
+      p2.y.should eq(5)
+    end
+    it "Si se multiplica por -1 se obtiene el opuesto" do
+      p2 = @p1*-1 
+      p2.x.should eq(-1)
+      p2.y.should eq(-1)
+    end
+  end
 end
